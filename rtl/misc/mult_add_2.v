2c0dbcf89bb91c0b1e3bcebce2d82cb023d92ad3
borti4938 <borti4938@gmail.com>
2023-09-03 01:07:56 +0200

    update on mult_add_2.v without functional change

--- rtl/misc/mult_add_2.v
+++ rtl/misc/mult_add_2.v
@@ -71,12 +71,14 @@ output [OUTPUT_DATA_W-1:0] result_data_o;
 localparam [1:0] inop_nop         = 2'b00;
 localparam [1:0] inop_normal      = 2'b01;
 localparam [1:0] inop_fir_mode    = 2'b10;
-localparam [1:0] inop_reserved     = 2'b11;
+localparam [1:0] inop_reserved    = 2'b11;
 
 localparam [1:0] calcop_normal    = 2'b00;
 localparam [1:0] calcop_bypass_a0 = 2'b01;
 localparam [1:0] calcop_bypass_a1 = 2'b10;
 localparam [1:0] calcop_reserved  = 2'b11;
+
+localparam pipeline_length = 2;
 
 // misc
 integer int_idx;
@@ -84,23 +86,28 @@ integer int_idx;
 // wires
 
 // registers
+reg [2:0] calcopcode_L[0:pipeline_length-1];
 reg [INPUT_DATA_A_W-1:0] data_a0_L, data_a1_L;
 reg [INPUT_DATA_B_W-1:0] data_b0_L, data_b1_L;
 
-reg [1:0] calcopcode_LL, calcopcode_L;
 reg [INPUT_DATA_A_W-1:0] bypass_mult_result;
-reg [INPUT_DATA_A_W+INPUT_DATA_B_W-1:0] bypass_result, mult_result_0, mult_result_1;
-reg [INPUT_DATA_A_W+INPUT_DATA_B_W:0] overall_result [0:POST_REGS];
+reg [INPUT_DATA_A_W+INPUT_DATA_B_W-1:0] mult_result_0, mult_result_1;
+
+reg [INPUT_DATA_A_W+INPUT_DATA_B_W:0] overall_result;
 
 // rtl
 
+// fetch inputs
 always @(posedge CLK_i or negedge nRST_i)
   if (!nRST_i) begin
     data_a0_L <= {INPUT_DATA_A_W{1'b0}};
     data_a1_L <= {INPUT_DATA_A_W{1'b0}};
-    data_b0_L <= {INPUT_DATA_B_W{1'b0}};
-    data_b1_L <= {INPUT_DATA_B_W{1'b0}};
+    for (int_idx = 0; int_idx < pipeline_length; int_idx = int_idx + 1)
+      calcopcode_L[int_idx] <= calcop_normal;
   end else begin
+    for (int_idx = 1; int_idx < pipeline_length; int_idx = int_idx + 1)
+      calcopcode_L[int_idx] <= calcopcode_L[int_idx-1];
+    calcopcode_L[0] <= calcopcode_i;
     case (inopcode_i)
       inop_normal: begin
           data_a0_L <= data_a0_i;
@@ -116,34 +123,46 @@ always @(posedge CLK_i or negedge nRST_i)
         end
     endcase
     data_b0_L <= data_b0_i;
-    data_b1_L <= data_b1_i;    
+    data_b1_L <= data_b1_i;
   end
 
+// processing pipeline
 always @(posedge CLK_i or negedge nRST_i)
   if (!nRST_i) begin
-    calcopcode_LL <= calcop_normal;
-    calcopcode_L <= calcop_normal;
     bypass_mult_result <= {INPUT_DATA_A_W{1'b0}};
     mult_result_0 <= {(INPUT_DATA_A_W+INPUT_DATA_B_W){1'b0}};
     mult_result_1 <= {(INPUT_DATA_A_W+INPUT_DATA_B_W){1'b0}};
-    overall_result[0] <= {(INPUT_DATA_A_W+INPUT_DATA_B_W+1){1'b0}};
-    for (int_idx = 0; int_idx < POST_REGS; int_idx = int_idx+1) begin
-      overall_result[int_idx+1] <= {(INPUT_DATA_A_W+INPUT_DATA_B_W+1){1'b0}};
-    end
+    overall_result <= {(INPUT_DATA_A_W+INPUT_DATA_B_W+1){1'b0}};
   end else begin
-    calcopcode_LL <= calcopcode_L;
-    calcopcode_L <= calcopcode_i;
-    bypass_mult_result <= calcopcode_L[1] ? data_a1_L : data_a0_L;
+    bypass_mult_result <= calcopcode_L[0] ? data_a1_L : data_a0_L;
 //    mult_result_0 <= data_a0_L * data_b0_L;
 //    mult_result_1 <= data_a1_L * data_b1_L;
     mult_result_0 <= data_a0_L * (* multstyle = "dsp" *) data_b0_L;
     mult_result_1 <= data_a1_L * (* multstyle = "dsp" *) data_b1_L;
-    overall_result[0] <= ^calcopcode_LL ? {1'b0,bypass_mult_result,{(INPUT_DATA_B_W){1'b0}}} : mult_result_0 + mult_result_1;
-    for (int_idx = 0; int_idx < POST_REGS; int_idx = int_idx+1) begin
-      overall_result[int_idx+1] <= overall_result[int_idx];
-    end
+    overall_result <= ^calcopcode_L[1] ? {1'b0,bypass_mult_result,{(INPUT_DATA_B_W){1'b0}}} : mult_result_0 + mult_result_1;
   end
 
-assign result_data_o = overall_result[POST_REGS][INPUT_DATA_A_W+INPUT_DATA_B_W-1:INPUT_DATA_A_W+INPUT_DATA_B_W-OUTPUT_DATA_W];
+// post regs and final assignment
+generate
+  if (POST_REGS > 0) begin
+    reg [OUTPUT_DATA_W-1:0] overall_result_L [1:POST_REGS];
+    
+    always @(posedge CLK_i or negedge nRST_i)
+      if (!nRST_i) begin
+        for (int_idx = 0; int_idx < POST_REGS; int_idx = int_idx+1) begin
+          overall_result_L[int_idx+1] <= {OUTPUT_DATA_W{1'b0}};
+        end
+      end else begin
+        for (int_idx = 0; int_idx < POST_REGS; int_idx = int_idx+1) begin
+          overall_result[int_idx+1] <= overall_result[int_idx];
+        end
+        overall_result_L[1] <= overall_result[INPUT_DATA_A_W+INPUT_DATA_B_W-1:INPUT_DATA_A_W+INPUT_DATA_B_W-OUTPUT_DATA_W];
+      end
+    
+    assign result_data_o = overall_result_L[POST_REGS];
+  end else begin
+    assign result_data_o = overall_result[INPUT_DATA_A_W+INPUT_DATA_B_W-1:INPUT_DATA_A_W+INPUT_DATA_B_W-OUTPUT_DATA_W];
+  end
+endgenerate
 
 endmodule
